ad024b073c0ba9df2f9f3c63a5ebb5c00df6da8c
Jermaine Jonas Ernst <34961982+jermainee@users.noreply.github.com>
2021-11-02 23:01:03 +0100

    Serve history via history.json

--- index.js
+++ index.js
@@ -6,13 +6,26 @@ const io = require('socket.io')(http,{
     }
 });
 const port = process.env.PORT || 3001;
+const messages = [];
 
 io.on('connection', (socket) => {
     socket.on('chat message', msg => {
         io.emit('chat message', msg);
+
+        messages.push(msg);
+
+        if (messages.length > 100) {
+            messages.shift();
+        }
+
+        console.log(msg);
     });
 });
 
 http.listen(port, () => {
     console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+
+app.get('/history.json', (req, res) => {
+    res.json(messages);
 });
