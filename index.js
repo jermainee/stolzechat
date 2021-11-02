7848038699bb9901afe4b9e3c06c1d28728e7476
Jermaine Jonas Ernst <34961982+jermainee@users.noreply.github.com>
2021-11-02 23:41:02 +0100

    Add static build serving to index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const origin = "http://localhost:3002";
+const origin = "http://localhost:3001";
 
-const app = require('express')();
+const express = require('express');
+const path = require('path');
+const app = express();
 const cors = require('cors');
 const http = require('http').Server(app);
 const io = require('socket.io')(http, { cors: { origin }});
@@ -21,8 +23,20 @@ io.on('connection', (socket) => {
     });
 });
 
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 http.listen(port, () => {
-    console.log(`Socket.IO server running at http://localhost:${port}/`);
+    console.log(`Socket.IO server running at :${port}`);
+});
+
+app.get('/status.json', cors({ origin }), (req, res) => {
+    const remoteAddress = req.socket.remoteAddress;
+
+    res.json({ success: remoteAddress.endsWith('.cpe.my-wire.de'), remoteAddress });
 });
 
 app.get('/history.json', cors({ origin }), (req, res) => {
