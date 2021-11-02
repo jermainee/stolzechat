ec43e21c09b2fec359940a3ba5482329d7eb1539
Jermaine Jonas Ernst <34961982+jermainee@users.noreply.github.com>
2021-11-03 00:41:24 +0100

    Add origin env

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const origin = "http://localhost:3001";
-
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -7,6 +5,7 @@ const cors = require('cors');
 const http = require('http').Server(app);
 const io = require('socket.io')(http, { cors: { origin }});
 const port = process.env.PORT || 3001;
+const origin = process.env.ORIGIN || 'http://localhost:3001';
 const messages = [];
 
 io.on('connection', (socket) => {
@@ -25,13 +24,6 @@ io.on('connection', (socket) => {
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function (req, res) {
-    const remoteAddress = req.socket.remoteAddress;
-
-    /*if (!remoteAddress.endsWith('.cpe.my-wire.de') && remoteAddress !== '::1') {
-        res.status(403).send('This website is only available from inside the StolzeHaus network');
-        return;
-    }*/
-
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 http.listen(port, () => {
@@ -39,12 +31,5 @@ http.listen(port, () => {
 });
 
 app.get('/history.json', cors({ origin }), (req, res) => {
-    const remoteAddress = req.socket.remoteAddress;
-
-    /*if (!remoteAddress.endsWith('.cpe.my-wire.de') && remoteAddress !== '::1') {
-        res.status(403).send(remoteAddress);//'This website is only available from inside the StolzeHaus network');
-        return;
-    }*/
-
     res.json(messages);
 });
